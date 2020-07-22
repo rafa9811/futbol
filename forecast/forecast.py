c707c31672007f40647b6a62d99cfae4b43d152e
Rafael Hidalgo Alejo <rafahidalgo@MBP-de-Rafael.home>
2020-07-22 11:39:29 +0200

    finished forecast

--- forecast/forecast.py
+++ forecast/forecast.py
@@ -1,22 +1,29 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import requests
 import xml.etree.ElementTree as et
 from datetime import datetime
 
-DRIVER_PATH = '/Users/rafahidalgo/Desktop/entornov/futbol/forecast/chromedriver'
+DRIVER_PATH = os.getcwd()[:-8] + 'chromedriver'
 options = Options()
 options.headless = True
 options.add_argument("--window-size=1920,1200")
 
 
-def get_id(municipio):
+def get_id(town):
     driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
     driver.get("http://www.aemet.es/es/portada")
 
-    driver.find_element_by_xpath("//*[@id='buscar_municipio']").send_keys(municipio)
+    driver.find_element_by_xpath("//*[@id='buscar_municipio']").send_keys(town)
     driver.find_element_by_xpath("//*[@id='columns']/div/div[2]/div[2]/div[1]/div/form/input[5]").click()
-    driver.find_element_by_xpath('//*[@class="resultados_busqueda"]/ul/li/a[text()="'+municipio+'"]').click()
+
+    if not driver.current_url.startswith('http://www.aemet.es/es/eltiempo/prediccion/municipios?modo=and&orden=n&tipo=sta&str='):
+        url = driver.current_url
+        id = url.split("id")[-1]
+        return id
+
+    driver.find_element_by_xpath('//*[@class="resultados_busqueda"]/ul/li/a[text()="'+town+'"]').click()
 
     url = driver.current_url
     id = url.split("id")[-1]
@@ -25,31 +32,41 @@ def get_id(municipio):
     return id
 
 
-def download_xml(municipio):
-    url = "http://www.aemet.es/xml/municipios/localidad_" + get_id(municipio) + ".xml"
+def download_xml(id):
+    url = "http://www.aemet.es/xml/municipios/localidad_" + id + ".xml"
     response = requests.get(url)
 
     with open('data.xml', 'wb') as file:
         file.write(response.content)
 
 
-def parse_xml(path):
-    tree = et.parse(path)
+def parse_xml(month, day):
+    tree = et.parse('data.xml')
     root = tree.getroot()
     prediction = root.find('prediccion')
 
     year = str(datetime.now().year)
-    mes = '07'
-    day = '27'
-    attr = year + '-' + mes + '-' + day
+    attr = year + '-' + str(month) + '-' + str(day)
 
     for child in prediction:
         if child.attrib['fecha'] == attr:
-            date = child
+            ddate = child
             break
         else: date = False
 
-    print(date.find('estado_cielo').text)
+    prob_precipitacion = ddate.find('prob_precipitacion').text
+    estado_cielo = ddate.find('estado_cielo').text
+    viento = ddate.find('viento')
+    vientodir = viento.find('direccion').text
+    vientovel = viento.find('velocidad').text
+    temperatura = ddate.find('temperatura')
+    tmax = temperatura.find('maxima').text
+    tmin = temperatura.find('minima').text
+
+    return prob_precipitacion, estado_cielo, vientodir, vientovel, tmax, tmin
 
 
-parse_xml('data.xml')
+def get_forecast(town, month, day):
+    id = get_id(town)
+    download_xml(id)
+    return parse_xml(month, day)
